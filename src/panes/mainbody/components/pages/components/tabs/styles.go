19d0017adb9fcf01b6e3f8ccccebc32ab24c3024
Liam Barrack <lbarto12@hotmail.com>
2026-04-20 01:34:11 -0400

    make tabs more tab-y

--- src/panes/mainbody/components/pages/components/tabs/styles.go
+++ src/panes/mainbody/components/pages/components/tabs/styles.go
@@ -6,21 +6,35 @@ type Styles struct {
 	tab func(m TabsModel, tab int) lipgloss.Style
 }
 
+func tabBorderWithBottom(left, middle, right string) lipgloss.Border {
+	border := lipgloss.RoundedBorder()
+	border.BottomLeft = left
+	border.Bottom = middle
+	border.BottomRight = right
+	return border
+}
+
 func MakeStyles() Styles {
+	inactiveTabBorder := tabBorderWithBottom("┴", "─", "┴")
+	activeTabBorder := tabBorderWithBottom("┘", " ", "└")
+
 	return Styles{
 		tab: func(m TabsModel, tab int) lipgloss.Style {
-			borderColor := lipgloss.Color("62")
-			if m.selected == tab {
-				borderColor = lipgloss.Color("205")
-			}
-
-			return lipgloss.NewStyle().
+			style := lipgloss.NewStyle().
 				Bold(true).
-				Border(lipgloss.RoundedBorder()).
+				Border(inactiveTabBorder).
 				Padding(0, 1).
 				Align(lipgloss.Center, lipgloss.Center).
-				BorderForeground(borderColor)
+				BorderForeground(lipgloss.Color("62"))
+
+			if m.selected == tab {
+				style = style.
+					Foreground(lipgloss.Color("205")).
+					Border(activeTabBorder)
+				// BorderForeground(lipgloss.Color("205"))
+			}
+
+			return style
 		},
 	}
 }
-
